76c7f7ada47ccec50ac4333856fc6b39a9e2d000
Tina Egolf <tinaegolf@gmail.com>
2015-10-13 14:25:36 +0200

    Added event handler for displaying selling points when scrolling

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,31 @@
-var animatePoints = function ( ) {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function (points) {
+    var revealPoint = function(i) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)"; 
+    };
     
-    var revealPoint = function(points) {
-        for (var i = 0; i < points.length; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)"; 
-        };
-    } 
-    
-    revealPoint(points);
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
 }; 
+
+window.onload = function () {
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    window.addEventListener("scroll", function(event){
+        if (document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
 
 
