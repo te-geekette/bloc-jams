8c767ec9add246bac5261a75ccd9f0ca348bc82b
Tina Egolf <tinaegolf@gmail.com>
2015-10-19 13:18:35 +0200

    Refactored Landingpage with JQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,26 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function (points) {
-    var revealPoint = function(i) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)"; 
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
     };
-    
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    $.each($('.point'), revealPoint);
+
 }; 
 
-window.onload = function () {
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+$(window).load(function () {
+    if ($(window).height() > 950) {
+        animatePoints();
     }
     
-    window.addEventListener("scroll", function(event){
-        if (document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+    $(window).scroll(function(event){
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
-}
+});
 
 
