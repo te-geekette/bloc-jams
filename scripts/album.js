21749100d36a22e7d2a9ee4625d077bb682b693a
Tina Egolf <tinaegolf@gmail.com>
2015-10-21 18:54:42 +0200

    Quick fix to get the song list back

--- scripts/album.js
+++ scripts/album.js
@@ -102,15 +102,17 @@ var updateSeekBarWhileSongPlays = function(){
     }
 };
 
-var updateSeekPercentage = function($seekBar, seekBarFillRatio){
-    var offsetXPercentage = seekBarFillRatio * 100;
-    offsetXPercentage = Math.max(0, offsetXPercentage);
-    offsetXPercentage = Math.min(100, offsetXPercentage);
-    
-    var percentageString = offsetXPercentage + %;
+var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+    var offsetXPercent = seekBarFillRatio * 100;
+
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(100, offsetXPercent);
+ 
+    var percentageString = offsetXPercent + '%';
     $seekBar.find('.fill').width(percentageString);
-    $seekBar.find('thumb').css({left: percentageString});
-};
+    $seekBar.find('.thumb').css({left: percentageString});
+ };
+
 
 var setupSeekBars = function(){
     var $seekBars = $('.player-bar .seek-bar');
